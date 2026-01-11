19ba179d361e8e648b63b59ec5e91ed80de2f022
padmshri <yadavpadmshri@gmail.com>
2026-01-11 15:58:58 +0530

    2,3 done

--- lab/02_strng-repl.c
+++ lab/02_strng-repl.c
@@ -1,43 +1,85 @@
-//function to find the first occurrence of a specific pattern (substring) within a larger string and replace it with a new replacement string.
-#include <stdio.h>
-#include <string.h>
+/*2. Develop a Program in C for the following operations on Strings.
+a. Read a main String (STR), a Pattern String (PAT) and a Replace String (REP)
+b. Perform Pattern Matching Operation: Find and Replace all occurrences of PAT in STR
+with REP if PAT exists in STR. Report suitable messages in case PAT does not exist in
+STR*/
 
-void read(char *s)
-{
+
+#include <stdio.h>
+// #include<conio.h>
+
+void read(char *s){
     gets(s);
 }
-           //copy s2 to s1
-void strcopy(char *s1,char *s2)
-{
+
+void my_strcpy(char *str1,char *str2){
     int i;
-    for(i=0;s2[i]!='\0';i++)
-        s1[i]=s2[i];
-    s1[i]='\0';
+    for(i=0; str2[i]!='\0';++i){
+        str1[i]=str2[i];
+    }
+    str1[i]='\0';
 }
 
-
-
-
+int matchnrplc(char *str,char *pat,char *rep){
+    char ans[100];
+    int flag =0;
+    int c=0,m=0,i=0,j=0;
+    while(str[c]!='\0'){
+        if(str[m]==pat[i]){
+            m++;
+            i++;
+            if(pat[i]=='\0'){
+                flag=1;
+                for(int k=0;rep[k]!='\0';++j,k++){
+                    ans[j]=rep[k];
+                    
+                }
+                i=0;
+                c=m;
+            }
+        }
+        else{
+            ans[j]=str[c];
+            j++;
+            c++;
+        
+        i=0;
+        m=c;
+        }
+    }
+    ans[j]='\0';
+    my_strcpy(str,ans);
+    return flag;
+    
+    
+}
 
 int main()
 {
     char str[100],pat[20],rep[20];
-    int flag=0;
+    int flag =0;
+    
     // clrscr();
     
-    printf("Enter the string");
+    printf("enter string :");
     read(str);
-    printf("Enter the pattern");
+    
+    printf("enter pattern :");
     read(pat);
-    printf("Enter the string to be replaced");
+    
+    printf("enter replace :");
     read(rep);
     
-    flag=matchnreplace(str,pat,rep);
-    
-    if(flag==1)
-        printf("The string is found %s",str);
-    else
-        printf("The pattern is not found");
+    flag = matchnrplc(str,pat,rep);
+    if(flag==0){
+        printf("not a match :");
+    }
+    else{
+        printf("a match :,%s",str);
         
+    }
+    
+    
+
     return 0;
 }
--- lab/03_stackOp.c
+++ lab/03_stackOp.c
@@ -1,1 +1,97 @@
+/*3. Develop a menu driven Program in C for the following operations on STACK of Integers
+(Array Implementation of Stack with maximum size MAX)
+a. Push an Element on to Stack
+b. Pop an Element from Stack
+c. Demonstrate how Stack can be used to check Palindrome
+d. Demonstrate Overflow and Underflow situations on Stack
+e. Display the status of Stack
+f. Exit
+*/
+
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define max 5
+
+int stack[max];
+int top =-1;
+
+void push( ){
+    int x;
+    if(top==max -1){
+        printf("overflow \n");
+    }
+    else{
+        printf("enter elemnt:");
+    scanf("%d",&x);
+        top ++;
+        stack[top]=x;
+        printf("pushed %d \n",x);
+    }
+
+}
+
+int pop(){
+    if(top == -1){
+        printf("underflow \n");
+    }
+    else{
+        int value = stack[top];
+        top--;
+        printf("poped %d \n",value);
+        return value;
+    }
+}
+
+void display(){
+    for (int i=0;i<= top;i++){
+        printf("%d ",stack[i]);
+    }
+    printf("\n");
+}
+
+void checkpalindrom(){
+    char  str[100];
+    int len ,i;
+    printf("enter string :");
+    scanf("%s",str);
+    int top=-1;
+
+    for(i=0;i< strlen(str);i++){
+        stack[++top]=str[i];
+    }
+
+   for(i=0;i<strlen(str);i++){
+        if(stack[top--]!=str[i]){
+            printf("%s not palindrom",str);
+            return ;
+        }
+        
+    }
+    printf("%s is a Palindrome\n", str);
+}
+
+int main(){
+    int ch;
+    while(1){
+        printf("push,pop,display,palindrom,exit \n");
+        printf("enter your choice:");
+        
+        scanf("%d",&ch);
+
+        switch(ch){
+            case 1:push(); break;
+            case 2: pop(); break;
+            case 3: display(); break;
+            case 4: checkpalindrom(); break;
+            case 5: exit(0);
+            default :
+            printf("invld");
+    
+
+
+        }
+    }
+
+    return 0;
+}
